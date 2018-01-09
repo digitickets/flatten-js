aa7bf7d529e8cd72f442ae05d8ff87ff8677c482
Richard Quadling <Richard.Quadling@DigiTickets.co.uk>
2018-01-09 17:37:25 +0000

    Add bool flattenArrays to flatten, allowing arrays to be not flattened. Add bool forceObject to unflatten, allowing numeric keys to cast as a string to allow objects with named properties.

--- src/flatten.js
+++ src/flatten.js
@@ -10,16 +10,18 @@
  *
  * @param {object} data
  * @param {string|null} numericPropertyPrefix If an object property is numeric, prefix the entry with this value.
+ * @param {boolean} flattenArrays If false any array encountered will be added to the output as-is, not flattened.
  * @returns {{}}
  */
-function flatten(data, numericPropertyPrefix) {
+function flatten(data, numericPropertyPrefix, flattenArrays) {
     var result = {};
     numericPropertyPrefix = !!numericPropertyPrefix ? numericPropertyPrefix : '~';
 
     function recurse(cur, prop) {
-        if (Object(cur) !== cur) {
+        var isArray = Array.isArray(cur);
+        if (Object(cur) !== cur || (isArray && flattenArrays === false)) {
             result[prop] = cur;
-        } else if (Array.isArray(cur)) {
+        } else if (isArray) {
             for (var i = 0, l = cur.length; i < l; i++) {
                 recurse(cur[i], prop ? prop + "." + i : "" + i);
             }
@@ -56,27 +58,29 @@ function flatten(data, numericPropertyPrefix) {
  * @param {string|null} numericPropertyPrefix Any object property keys prefixed with this value will be treated as a string/property and not an array index.
  * @param {string[]|null} separators
  * @param {string|null} defaultSeparator
+ * @param {boolean} forceObject All numeric keys will be treated as a string to force the result to be a nested object, not a nested array.
  * @returns {*}
  */
-function unflatten(data, numericPropertyPrefix , separators, defaultSeparator) {
+function unflatten(data, numericPropertyPrefix , separators, defaultSeparator, forceObject) {
     if (Object(data) !== data || Array.isArray(data)) {
         return data;
     }
     numericPropertyPrefix = !!numericPropertyPrefix ? numericPropertyPrefix : '~';
     defaultSeparator = !!defaultSeparator ? defaultSeparator : '.';
     separators = !!separators ? separators : [];
+    forceObject = !!forceObject;
     var result = {}, cur, prop, idx, last, temp;
     for (var p in data) {
         var count = 0;
-        cur = result, prop = "", last = 0;
+        cur = result, prop = '', last = 0;
         do {
             idx = p.indexOf(count < separators.length ? separators[count++] : defaultSeparator, last);
             temp = p.substring(last, idx !== -1 ? idx : undefined);
-            cur = cur[prop] || (cur[prop] = (!isNaN(parseInt(temp)) ? [] : {}));
-            prop = (p.indexOf(numericPropertyPrefix) != -1) ? temp.replace(numericPropertyPrefix, '').toString() : temp;
+            cur = cur[prop] || (cur[prop] = ((!forceObject && !isNaN(parseInt(temp))) ? [] : {}));
+            prop = (p.indexOf(numericPropertyPrefix) !== -1) ? temp.replace(numericPropertyPrefix, '').toString() : temp;
             last = idx + 1;
         } while (idx >= 0);
-        cur[prop] = data[p];
+        cur[forceObject ? prop.toString() : prop] = data[p];
     }
     return result[""];
 }
